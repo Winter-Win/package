3771d3062968c3bc3158dbcdef0e2c2d58093808
Winter-Win <669886902@qq.com>
2019-03-24 22:31:42 +0800

    2019/3/24(1)

---
+++ ConcurrentMemoryPool/ConcurrentMemoryPool/CentralCache.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Common.h"
+
+//上面的ThreadCache里面没有的话，要从中心获取
+//设计成单例模式
+class CentralCache
+{
+public:
+	static CentralCache* Getinstance()
+	{
+		return &_inst;
+	}
+
+	//从page cache获取一个span
+	Span* GetOneSpan(SpanList& spanlist, size_t byte_size);
+
+	//从中心缓存获取一定数量的对象给threa cache
+	size_t FetchRangeObj(void*& start, void*& end, size_t n, size_t byte_size);
+
+	//将一定数量的对象释放给span跨度
+	void ReleaseListToSpans(void* start, size_t size);
+
+private:
+	SpanList _spanlist[NLISTS];
+
+private:
+	CentralCache(){}//声明不实现，防止默认构造，自己创建
+
+	CentralCache(CentralCache&) = delete;
+	static CentralCache _inst;
+};
---
+++ ConcurrentMemoryPool/ConcurrentMemoryPool/PageCache.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Common.h"
+
+//单例模式
+class PageCache
+{
+public:
+	static PageCache* GetInstence()
+	{
+		return &_inst;
+	}
+
+	Span* NewSpan(size_t n);//获取的是以页为单位
+
+	//获取从对象到span的映射
+	Span* MapObjectToSpan(void* obj);
+
+	//释放空间span回到PageCache，并合并相邻的span
+	void ReleaseSpanToPageCache(Span* span);
+
+private:
+	SpanList _spanlist[NPAGES];
+	std::map<PageID, Span*> _idspanmap;
+private:
+	PageCache(){}
+
+	PageCache(const PageCache&) = delete;
+	static PageCache _inst;
+};
