fdd6828e430f8678eaa415cfaf3f2e34109b91ee
Winter-Win <669886902@qq.com>
2019-05-25 11:42:36 +0800

    进制转换

--- OJ/OJ2.cpp
+++ OJ/OJ2.cpp
@@ -124,3 +124,52 @@ int main() {
         help(n, m, v, 1);
     }
 }
+
+
+4、进制转换
+https://www.nowcoder.com/practice/ac61207721a34b74b06597fe6eb67c52?tpId=85&&tqId=29862&rp=1&ru=/activity/oj&qru=/ta/2017test/question-ranking
+
+题目描述
+给定一个十进制数M，以及需要转换的进制数N。将十进制数M转化为N进制数
+输入描述:
+输入为一行，M(32位整数)、N(2 ≤ N ≤ 16)，以空格隔开。
+输出描述:
+为每个测试实例输出转换后的数，每个输出占一行。如果N大于9，则对应的数字规则参考16进制（比如，10用A表示，等等）
+示例1
+输入
+7 2
+输出
+111
+
+
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+string table = "0123456789ABCDEF";
+
+int main()
+{
+    string s;
+    int m, n;
+    cin >> m >> n;
+    bool flag = false;
+    // 如果是负数，则转成正数，并标记一下
+    if(m < 0)
+    {
+        m = 0 - m;
+        flag = true;
+    }
+    // 按进制换算成对应的字符添加到s
+    while(m)
+    {
+        s += table[m % n];
+        m /= n;
+    }
+    if(flag)
+        s += "-";
+    reverse(s.begin(), s.end());
+    cout << s << endl;
+    return 0;
+}
