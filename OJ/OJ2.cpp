c3805ecd6f041f4a5e72f19cc4ea4cc17b7d43d0
Winter-Win <669886902@qq.com>
2019-05-29 21:47:11 +0800

    两种排序方法

--- OJ/OJ2.cpp
+++ OJ/OJ2.cpp
@@ -221,3 +221,79 @@ int main()
 	system("pause");
 	return 0;
 }
+
+
+6、两种排序方法
+https://www.nowcoder.com/practice/839f681bf36c486fbcc5fcb977ffe432?tpId=85&&tqId=29844&rp=1&ru=/activity/oj&qru=/ta/2017test/question-ranking
+
+题目描述
+考拉有n个字符串字符串，任意两个字符串长度都是不同的。考拉最近学习到有两种字符串的排序方法： 1.根据字符串的字典序排序。例如：
+"car" < "carriage" < "cats" < "doggies < "koala"
+2.根据字符串的长度排序。例如：
+"car" < "cats" < "koala" < "doggies" < "carriage"
+考拉想知道自己的这些字符串排列顺序是否满足这两种排序方法，考拉要忙着吃树叶，所以需要你来帮忙验证。
+输入描述:
+输入第一行为字符串个数n(n ≤ 100)
+接下来的n行,每行一个字符串,字符串长度均小于100，均由小写字母组成
+输出描述:
+如果这些字符串是根据字典序排列而不是根据长度排列输出"lexicographically",
+
+如果根据长度排列而不是字典序排列输出"lengths",
+
+如果两种方式都符合输出"both"，否则输出"none"
+示例1
+输入
+3
+a
+aa
+bbb
+输出
+both
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool IsDictSort(const vector<string>& vs)
+{
+    for(size_t i = 1; i < vs.size(); ++i)
+    {
+        if(vs[i] < vs[i-1])
+            return false;
+    }
+    return true;
+}
+
+bool IsLengthsSort(const vector<string>& vs)
+{
+    for(size_t i = 1; i < vs.size(); ++i)
+    {
+        if(vs[i].size() < vs[i-1].size())
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    while(cin >> n)
+    {
+        vector<string> vs(n);
+        for(int i = 0; i < n; ++i)
+            cin >> vs[i];
+        
+        if(IsDictSort(vs) && IsLengthsSort(vs))
+            cout << "both" << endl;
+        else if(IsDictSort(vs))
+            cout << "lexicographically" << endl;
+        else if(IsLengthsSort(vs))
+            cout << "lengths" << endl;
+        else
+            cout << "none" << endl;
+    }
+    return 0;
+}
