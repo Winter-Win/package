f6665672793871bfc3b1c04706489e1ebecb0c0a
Winter-Win <669886902@qq.com>
2019-07-04 22:58:50 +0800

    星际密码

--- OJ/OJ4.cpp
+++ OJ/OJ4.cpp
@@ -78,7 +78,8 @@ int main()
 }
 
 
-2、删除链表中等于给定值 val 的所有节点。https://leetcode-cn.com/problems/remove-linked-list-elements/
+2、删除链表中等于给定值 val 的所有节点。
+https://leetcode-cn.com/problems/remove-linked-list-elements/
 示例:
 输入: 1->2->6->3->4->5->6, val = 6
 输出: 1->2->3->4->5
@@ -181,6 +182,62 @@ int main()
         }
         //对总和求树根
         cout << numRoot(sum) << endl;
+    }
+    return 0;
+}
+
+4、星际密码
+https://www.nowcoder.com/questionTerminal/34f17d5f2a8240bea661a23ec095a062
+链接：https://www.nowcoder.com/questionTerminal/34f17d5f2a8240bea661a23ec095a062
+来源：牛客网
+
+星际战争开展了100年之后，NowCoder终于破译了外星人的密码！他们的密码是一串整数，通过一张表里的信息映射成最终4位密码。表的规则是：n对应的值是矩阵X的n次方的左上角，如果这个数不足4位则用0填充，如果大于4位的则只输出最后4位。
+|1 1|^n => |Xn ..|
+|1 0|      |.. ..|
+例如n=2时，
+|1 1|^2 => |1 1| * |1 1| => |2 1|
+|1 0|      |1 0|   |1 0|    |1 1|
+即2对应的数是“0002”。
+
+输入描述:
+输入有多组数据。
+每组数据两行：第一行包含一个整数n (1≤n≤100)；第二行包含n个正整数Xi (1≤Xi≤10000)
+
+
+输出描述:
+对应每一组输入，输出一行相应的密码。
+示例1
+输入
+6
+18 15 21 13 25 27
+5
+1 10 100 1000 10000
+输出
+418109877711037713937811
+00010089410135017501
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> a = {1,1};
+void data_init()
+{
+    int i;
+    for(i=2;i<10005;i++){
+        a.push_back((a[i-1]+a[i-2]) % 10000);
+    }
+}
+int main()
+{
+    int n,t;
+    data_init();
+    while(std::cin >> n){
+        while(n--){
+            cin >> t;
+            printf("%04d",a[t]);
+        }
+        printf("\n");
     }
     return 0;
 }
