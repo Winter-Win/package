43237d9a06a8c6be31828b3f6e4fa1e0e790b627
Winter-Win <669886902@qq.com>
2019-07-10 18:03:24 +0800

    斐波那契凤尾

--- OJ/OJ4.cpp
+++ OJ/OJ4.cpp
@@ -495,3 +495,70 @@ int main() {
     return 0;
 }
 
+
+9、斐波那契凤尾
+https://www.nowcoder.com/questionTerminal/c0a4b917a15f40a49ca10532ab9019fb
+
+链接：https://www.nowcoder.com/questionTerminal/c0a4b917a15f40a49ca10532ab9019fb
+来源：牛客网
+
+NowCoder号称自己已经记住了1-100000之间所有的斐波那契数。
+为了考验他，我们随便出一个数n，让他说出第n个斐波那契数。当然，斐波那契数会很大。因此，如果第n个斐波那契数不到6位，则说出该数；否则只说出最后6位。
+
+输入描述:
+输入有多组数据。
+每组数据一行，包含一个整数n (1≤n≤100000)。
+
+
+输出描述:
+对应每一组输入，输出第n个斐波那契数的最后6位。
+示例1
+输入
+1
+2
+3
+4
+100000
+输出
+1
+2
+3
+5
+537501
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Fib[100001] = {0};
+    Fib[0] = 1;
+    Fib[1] = 1;
+    for(int i = 2; i < 100001; ++i)
+    {
+        // 计算所有的斐波那契数
+        Fib[i] = Fib[i-1] + Fib[i-2];
+        //  提前处理只保留后六位
+        Fib[i] %= 1000000;
+    }
+    //  记录分界值
+    int ret = 0;
+    for(ret = 2; ret < 100001; ++ret)
+    {
+        if(Fib[ret] >= 100000)
+            break;
+    }
+    //  循环输入
+    int n;
+    while(cin >> n)
+    {
+        //  原因是在开始报尾数六位的时候，很有可能因为前面的取模运算
+        //  数字不够六位，所以拆分后进行前面补0
+        if(n < ret)
+            printf("%d\n", Fib[n]);
+        else
+            printf("%06d\n", Fib[n]);
+    }
+    
+    return 0;
+}
