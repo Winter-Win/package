0b7414c858c78ab60c7686f918edc7e1f1ed4c71
Winter-Win <669886902@qq.com>
2019-08-19 22:01:22 +0800

    逆波兰表达式求值

--- OJ/OJ5.cpp
+++ OJ/OJ5.cpp
@@ -135,3 +135,76 @@ int main()
 	}
 	return 0;
 }
+
+
+
+4、逆波兰表达式求值（150）
+根据逆波兰表示法，求表达式的值。
+有效的运算符包括?+,?-,?*,?/?。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+说明：
+整数除法只保留整数部分。
+给定逆波兰表达式总是有效的。换句话说，表达式总会得出有效数值且不存在除数为 0 的情况。
+示例?1：
+
+输入: ["2", "1", "+", "3", "*"]
+输出: 9
+解释: ((2 + 1) * 3) = 9
+示例?2：
+
+输入: ["4", "13", "5", "/", "+"]
+输出: 6
+解释: (4 + (13 / 5)) = 6
+示例?3：
+
+输入: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+输出: 22
+解释: 
+  ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+
+
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+    int sum = 0, left = 0, right = 0;
+	stack<int> st;
+	for (size_t i = 0; i < tokens.size(); ++i)
+	{
+		string ret = tokens[i];
+		if (isdigit(ret[0]) || isdigit(ret[1]))  // 判断是否数字  注意  可能是负数
+			st.push(atoi(ret.c_str()));
+		else
+		{
+				right = st.top();
+				st.pop();
+				left = st.top();
+				st.pop();
+				switch (ret[0])
+				{
+				case '+':
+					sum = left + right;
+					break;
+				case '-':
+					sum = left - right;
+					break;
+				case '*':
+					sum = left * right;
+					break;
+				case '/':
+					sum = left / right;
+					break;
+				default:
+					break;
+				}
+				st.push(sum);
+		}
+	}
+	return st.top();
+    }
+};
